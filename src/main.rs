77a3d868fea39ac3f764a933adf626c4454aa60f
Travis Wagner <travis.wagner@umusic.com>
2023-03-16 02:23:27 -0400

    add dynamic routing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 use std::process;
 use std::{
@@ -19,6 +20,30 @@ fn main() {
     }
 }
 
+type Handler = fn() -> String;
+
+/// Gets the route from the request string, e.g. "/foo/bar?baz=qux" -> "/foo/bar"
+///
+/// # Examples
+///
+/// ```
+/// let request = "GET /foo/bar?baz=qux HTTP/1.1";
+/// let route = get_route(request);
+/// assert_eq!(route, "/foo/bar");
+/// ```
+fn get_route(request: &str) -> String {
+    let mut parts = request.split_whitespace();
+    let _method = parts.next().unwrap();
+    let route = parts.next().unwrap();
+    let _version = parts.next().unwrap();
+
+    let mut route_parts = route.split('?');
+    let route = route_parts.next().unwrap();
+
+    route.to_string()
+}
+
+/// Handles a connection, reading the request and writing the response.
 fn handle_connection(mut stream: TcpStream) {
     let buf = BufReader::new(&mut stream);
     let req: Vec<_> = buf
@@ -27,43 +52,82 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    let route = "GET / HTTP/1.1";
-    if req.first().is_some() && req[0] == route {
-        println!("Request {:#?}", req);
-        let body = match fs::read_to_string("src/index.html") {
-            Ok(r) => r,
-            Err(err) => {
-                println!("{err}");
-                return;
-            }
-        };
-        let status = "HTTP/1.1 200 OK \r\n";
-        let size = format!("Content-Length: {}\r\n", body.len());
-        let response = format!("{status}{size}\r\n{body}");
-        match stream.write_all(response.as_bytes()) {
-            Ok(r) => r,
-            Err(err) => {
-                println!("{err}");
-                return;
-            }
+    let mut status = "HTTP/1.1 200 OK \r\n";
+    let request_string = match req.first() {
+        Some(r) => r.to_string(),
+        None => {
+            println!("Request {req:#?}");
+            status = "HTTP/1.1 404 Not Found \r\n";
+            "/404".to_string()
         }
-    } else {
-        let status = "HTTP/1.1 404 Not Found \r\n";
-        let body = match fs::read_to_string("src/404.html") {
-            Ok(r) => r,
-            Err(err) => {
-                println!("{err}");
-                return;
-            }
-        };
-        let size = format!("Content-Length: {}\r\n", body.len());
-        let response = format!("{status}{size}\r\n{body}");
-        match stream.write_all(response.as_bytes()) {
-            Ok(r) => r,
-            Err(err) => {
-                println!("{err}");
-                return;
-            }
+    };
+    let request_route = get_route(&request_string);
+
+    let mut handlers: HashMap<&str, Handler> = HashMap::new();
+    handlers.insert("/", index);
+
+    let handler = match handlers.get(request_route.as_str()) {
+        Some(h) => h.to_owned(),
+        None => {
+            println!("Request {req:#?}");
+            status = "HTTP/1.1 404 Not Found \r\n";
+            not_found
         }
+    };
+
+    println!("Request {req:#?}");
+    let body = handler();
+    let size = format!("Content-Length: {}\r\n", body.len());
+    let response = format!("{status}{size}\r\n{body}");
+    match stream.write_all(response.as_bytes()) {
+        Ok(r) => r,
+        Err(err) => {
+            println!("{err}");
+        }
+    }
+    println!("Request {:#?}", req[0]);
+}
+
+fn index() -> String {
+    match fs::read_to_string("src/index.html") {
+        Ok(r) => r,
+        Err(err) => {
+            println!("{err}");
+            "".to_string()
+        }
+    }
+}
+
+fn not_found() -> String {
+    match fs::read_to_string("src/404.html") {
+        Ok(r) => r,
+        Err(err) => {
+            println!("{err}");
+            "".to_string()
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_route() {
+        let request = "GET / HTTP/1.1";
+        let route = get_route(request);
+        assert_eq!(route, "/");
+
+        let request = "GET /foo HTTP/1.1";
+        let route = get_route(request);
+        assert_eq!(route, "/foo");
+
+        let request = "GET /foo/bar HTTP/1.1";
+        let route = get_route(request);
+        assert_eq!(route, "/foo/bar");
+
+        let request = "GET /foo/bar?baz=qux HTTP/1.1";
+        let route = get_route(request);
+        assert_eq!(route, "/foo/bar");
     }
 }
